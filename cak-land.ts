fe1d86c50d70768e656631dd6b6bc320969028d0
rweir <rweir@inksmith.ca>
2021-10-13 10:58:10 -0400

    Rewrote motor to control speed with voltage and on/off by toggling PWM state

--- cak-land.ts
+++ cak-land.ts
@@ -1,7 +1,7 @@
 //% weight=100 color=#421C52 icon="\uf1b9"
 namespace cakLand {
-    export let M2_PWR: number = DigitalPin.P13
-    export let M2_DIR: number = DigitalPin.P14
-    export let M1_PWR: number = DigitalPin.P15
-    export let M1_DIR: number = DigitalPin.P16
+    export let M2_NEG: number = DigitalPin.P13
+    export let M2_POS: number = DigitalPin.P14
+    export let M1_NEG: number = DigitalPin.P15
+    export let M1_POS: number = DigitalPin.P16
 }
